62d9a140c0784eac9f7bd3daa539be760151ae93
Ruben Bezuidenhout <rex22@live.co.za>
2019-10-27 16:20:02 +0200

    Loading products from the database

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var Product = require("../models/product");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("shop/index", { title: "Sword Shop" });
+  Product.getProducts().then(result => {
+    res.render("shop/index", { title: "Sword Shop", products: result });
+  });
 });
 
 /* GET about us page. */
