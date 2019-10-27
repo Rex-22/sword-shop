076eaf932e44b0e24e31f3d993eb5fa84126b54d
Ruben Bezuidenhout <rex22@live.co.za>
2019-10-27 11:49:21 +0200

    Using HBS view engine.
    Also added pages from the old website

--- app/app.js
+++ app/app.js
@@ -1,20 +1,53 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var expressHbs = require("express-handlebars");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require("./routes/");
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// View engine setup
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", routes);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Development error handler
+// Will print stacktrace
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
