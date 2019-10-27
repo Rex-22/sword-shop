897a3ddccc5d6d7641496508353614f71ee0ad98
Ruben Bezuidenhout <rex22@live.co.za>
2019-10-27 17:34:43 +0200

    Added popup message for contact us form

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,7 +16,11 @@ router.get("/about_us", function(req, res, next) {
 
 /* GET about us page. */
 router.get("/contact_us", function(req, res, next) {
-  res.render("shop/contact_us", { title: "About Us" });
+    res.render("shop/contact_us", { title: "Contact Us" });
+});
+
+router.post("/contact_us", function(req, res, next) {
+    res.render("shop/contact_us", { title: "Contact Us", showMessage: true});
 });
 
 module.exports = router;
