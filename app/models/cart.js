76b3b649fb9de96ba778e42c4152770d46934556
Ruben Bezuidenhout <rex22@live.co.za>
2019-10-28 01:11:00 +0200

    Updated cart

--- app/models/cart.js
+++ app/models/cart.js
@@ -1,35 +1,55 @@
 module.exports = class Cart {
-    items;
-    totalQty;
-    totalPrice;
+  items;
+  totalQty;
+  totalPrice;
 
-    /**
+  /**
      * @constructor
      * @param {Array} oldCart The items from the previous cart. Just pass an empty array if this is a new cart
      */
-    constructor(oldCart) {
-        this.items = oldCart.items || {};
-        this.totalQty = oldCart.totalQty || 0;
-        this.totalPrice = oldCart.totalPrice || 0;
-    }
+  constructor(oldCart) {
+    this.items = oldCart.items || {};
+    this.totalQty = oldCart.totalQty || 0;
+    this.totalPrice = oldCart.totalPrice || 0;
+  }
 
-    add = (item, id) => {
-        var storedItem = this.items[id];
-        if (!storedItem) {
-            storedItem = this.items[id] = {item: item, qty: 0, price: 0};
-        }
-        storedItem.qty++;
-        storedItem.price = storedItem.item.price * storedItem.qty;
-        this.totalQty++;
-        this.totalPrice += storedItem.item.price;
+  /**
+     * @param {Product} item The item that we want to add
+     * @param {Number} id The id of the product
+     */
+  add = (item, id) => {
+    var storedItem = this.items[id];
+    if (!storedItem) {
+      storedItem = this.items[id] = {
+        item: item,
+        qty: 0,
+        price: 0
+      };
     }
+    storedItem.qty++;
+    storedItem.price = storedItem.item.price * storedItem.qty;
+    this.totalQty++;
+    this.totalPrice += storedItem.item.price;
+  };
 
-    generateArray = () => {
-        var arr = [];
-        for(var id in this.items) {
-            arr.push(this.items[id]);
-        }
-        return arr;
+  /**
+     * @param {Number} id The id of the product
+     */
+  remove = id => {
+    var storedItem = this.items[id];
+    storedItem.qty--;
+    storedItem.price = storedItem.item.price * storedItem.qty;
+    this.totalQty--;
+    this.totalPrice -= storedItem.item.price;
+    if (storedItem.qty === 0) 
+      delete this.items[id];
+    };
+  
+  generateArray = () => {
+    var arr = [];
+    for (var id in this.items) {
+      arr.push(this.items[id]);
     }
-
-}
+    return arr;
+  };
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,61 +3,88 @@ var router = express.Router();
 var Product = require("../models/product");
 var Cart = require("../models/cart");
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   Product.getProducts().then(result => {
-    res.render("shop/index", { title: "Sword Shop", products: result });
+    res.render("shop/index", {
+      title: "Sword Shop",
+      products: result
+    });
   });
 });
 
-/* GET about us page. */
-router.get("/about-us", function(req, res, next) {-
-  res.render("shop/about_us", { title: "About Us" });
+router.get("/about-us", function (req, res, next) {-res.render("shop/about_us", {title: "About Us"});
 });
 
-/* GET contact us page. */
-router.get("/contact-us", function(req, res, next) {
-    res.render("shop/contact_us", { title: "Contact Us" });
+router.get("/contact-us", function (req, res, next) {
+  res.render("shop/contact_us", {title: "Contact Us"});
 });
 
-/* GET cart page. */
-router.get("/cart", function(req, res, next) {
-    if (!req.session.cart) {
-        return res.render("shop/cart",  { title: "Cart", products: null})
-    }
-    var cart = new Cart(req.session.cart);
-    res.render("shop/cart", { title: "Cart", products: cart.generateArray(), totalPrice: cart.totalPrice});
-});
-
-/* POST data from contact us page. */
-router.post("/contact-us", function(req, res, next) {
-    res.render("shop/contact_us", { title: "Contact Us", showMessage: true});
-});
-
-/* POST data from contact us page. */
-router.get("/add-to-cart/:id", function(req, res, next) {
-    var productId = req.params.id;
-    var cart = new Cart(req.session.cart ? req.session.cart : {});
-
-    Product.getProduct(productId).then((result) => {
-        cart.add(result, result.id);
-        req.session.cart = cart;
-        console.log(req.session.cart);
-        res.redirect('/');
-    }).catch((err) => {
-        console.log(err);
-        return res.redirect('/');
+router.get("/cart", function (req, res, next) {
+  if (!req.session.cart) {
+    return res.render("shop/cart", {
+      title: "Cart",
+      products: null
     });
+  }
+  var cart = new Cart(req.session.cart);
+  res.render("shop/cart", {
+    title: "Cart",
+    products: cart.generateArray(),
+    totalPrice: cart.totalPrice
+  });
 });
 
-router.get("/checkout", function(req, res, next) {
-    if (req.session.cart) {
-        res.render("shop/checkout");
-    } else {
-        res.render("error", {message: "No items in cart", error: {status: "There are no items in your cart. Add some before checking out!"}})
-        //TODO Save cart to database
-        //TODO Users.... we need them
-    }
-})
+router.post("/contact-us", function (req, res, next) {
+  res.render("shop/contact_us", {
+    title: "Contact Us",
+    showMessage: true
+  });
+});
+
+router.get("/add-to-cart/:id", function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(
+    req.session.cart
+    ? req.session.cart
+    : {});
+
+  Product.getProduct(productId).then(result => {
+    cart.add(result, result.id);
+    req.session.cart = cart;
+    res.redirect("/cart");
+  }).catch(err => {
+    console.log(err);
+    return res.redirect("/");
+  });
+});
+
+router.get("/remove-from-cart/:id", function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart);
+
+  Product.getProduct(productId).then(result => {
+    cart.remove(result.id);
+    req.session.cart = cart;
+    res.redirect("/cart");
+  }).catch(err => {
+    console.log(err);
+    return res.redirect("/");
+  });
+});
+
+router.get("/checkout", function (req, res, next) {
+  if (req.session.cart) {
+    res.render("shop/checkout");
+  } else {
+    res.render("error", {
+      message: "No items in cart",
+      error: {
+        status: "There are no items in your cart. Add some before checking out!"
+      }
+    });
+    //TODO Save cart to database
+    //TODO Users.... we need them
+  }
+});
 
 module.exports = router;
