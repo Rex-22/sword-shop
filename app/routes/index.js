70430380d077ec6bdc08a71e1262481ff05dbcb5
Ruben Bezuidenhout <rex22@live.co.za>
2019-10-27 13:32:15 +0200

    Added about us and contact us pages

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Sword Shop" });
+  res.render("shop/index", { title: "Sword Shop" });
+});
+
+/* GET about us page. */
+router.get("/about_us", function(req, res, next) {
+  res.render("shop/about_us", { title: "About Us" });
+});
+
+/* GET about us page. */
+router.get("/contact_us", function(req, res, next) {
+  res.render("shop/contact_us", { title: "About Us" });
 });
 
 module.exports = router;
