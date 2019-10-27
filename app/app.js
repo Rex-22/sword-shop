a771b62fdf3225d61e35b0b96fec898d91e761c9
Ruben Bezuidenhout <rex22@live.co.za>
2019-10-27 22:16:05 +0200

    Added sessions and cookies

--- app/app.js
+++ app/app.js
@@ -5,21 +5,52 @@ var favicon = require("serve-favicon");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var expressHbs = require("express-handlebars");
+var session = require("express-session");
+var flash = require('connect-flash');
+var MySQLStore = require('express-mysql-session')(session);
+var database = require('./middleware/database')
 
 var routes = require("./routes/");
 
 var app = express();
-app.use(favicon(path.join(__dirname, "public/images", "favicon.ico")));
+
+var sessionOptions = {
+    expiration: 180 * 60 * 1000,
+    schema: {
+        tableName: 'sessions',
+        columnNames: {
+            session_id: 'session_id',
+            expires: 'expires',
+            data: 'data'
+        }
+    }
+}
+
+var sessionStore = new MySQLStore(sessionOptions, database);
 
 // View engine setup
 app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+app.use(favicon(path.join(__dirname, "public/images", "favicon.ico")));
+app.use(session({
+    key: 'sword_store',
+    secret:'dontcare', 
+    store: sessionStore,
+    resave: false, 
+    saveUninitialized: false
+}));
+app.use(flash());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
 
 app.use("/", routes);
 
