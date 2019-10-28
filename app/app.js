4b54a171450ad1dfdb2ee0e43866b25b3c6acff7
Ruben Bezuidenhout <rex22@live.co.za>
2019-10-28 11:08:58 +0200

    Added sign in page

--- app/app.js
+++ app/app.js
@@ -6,56 +6,52 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var expressHbs = require("express-handlebars");
 var session = require("express-session");
-var flash = require('connect-flash');
-var MySQLStore = require('express-mysql-session')(session);
-var database = require('./middleware/database')
+var flash = require("connect-flash");
+var MySQLStore = require("express-mysql-session")(session);
+var database = require("./middleware/database");
 
 var routes = require("./routes/");
+var userRouter = require("./routes/users");
 
 var app = express();
 
 var sessionOptions = {
-    expiration: 180 * 60 * 1000,
-    schema: {
-        tableName: 'sessions',
-        columnNames: {
-            session_id: 'session_id',
-            expires: 'expires',
-            data: 'data'
-        }
+  expiration: 180 * 60 * 1000,
+  schema: {
+    tableName: "sessions",
+    columnNames: {
+      session_id: "session_id",
+      expires: "expires",
+      data: "data"
     }
-}
+  }
+};
 
 var sessionStore = new MySQLStore(sessionOptions, database);
 
 // View engine setup
-app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
+app.engine(".hbs", expressHbs({defaultLayout: "layout", extname: ".hbs"}));
 app.set("view engine", ".hbs");
 
 app.use(favicon(path.join(__dirname, "public/images", "favicon.ico")));
-app.use(session({
-    key: 'sword_store',
-    secret:'dontcare', 
-    store: sessionStore,
-    resave: false, 
-    saveUninitialized: false
-}));
+app.use(session({key: "sword_store", secret: "dontcare", store: sessionStore, resave: false, saveUninitialized: false}));
 app.use(flash());
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(function(req, res, next) {
-    res.locals.session = req.session;
-    next();
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
 });
 
 app.use("/", routes);
+app.use("/user", userRouter);
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
@@ -64,7 +60,7 @@ app.use(function(req, res, next) {
 // Development error handler
 // Will print stacktrace
 if (app.get("env") === "development") {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
       message: err.message,
@@ -75,7 +71,7 @@ if (app.get("env") === "development") {
 
 // Production error handler
 // No stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error", {
     message: err.message,
